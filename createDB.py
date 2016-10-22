e2079cc4adab6b60ac018348f26fd9fb9e40ed70
jakegornall <jakegornall@yahoo.com>
2016-10-22 15:55:20 -0400

    Update

--- createDB.py
+++ createDB.py
@@ -1,25 +1,44 @@
-import sqlite3
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
 
-# name your database
-database = 'ItemCatalog.db'
+Base = declarative_base()
 
-with open('databaseSetupScript.sql', 'r') as databaseSetupScript:
-	databaseSetupScript = databaseSetupScript.read().replace('\n', '').replace('\t', '')
+class Users(Base):
+	__tablename__ = 'Users'
+	id = Column(Integer, primary_key=True)
+	name = Column(String(80), nullable=False)
+	email = Column(String(255), nullable=True)
+	profilePicURL = Column(String(255), default="http://placehold.it/200x200")
+	
+class Items(Base):
+	__tablename__ = 'Items'
+	id = Column(Integer, primary_key=True)
+	name = Column(String(80), nullable=False)
+	sellerID = Column(Integer, ForeignKey("Users.id"))
+	description = Column(String(255), nullable=False)
+	price = Column(Integer, nullable=False)
+	onSale = Column(String(5), default="False")
+	onClearance = Column(String(5), default="False")
+	imageURL = Column(String(255), nullable=False)
+	qty = Column(Integer, default=0)
 
-if not databaseSetupScript:
-	print "database setup script unavailable."
+	def serialize(self):
+		return {
+			'id': self.id,
+			'name': self.name,
+			'sellerID': self.sellerID,
+			'description': self.description,
+			'price': self.price,
+			'onSale': self.onSale,
+			'onClearance': self.onClearance,
+			'imageURL': self.imageURL,
+			'qty': self.qty
+		}
 
-else:
-	DBactionList = databaseSetupScript.split(';')
-	print DBactionList
 
-	for i in DBactionList:
-		conn = sqlite3.connect(database)
-		if not conn:
-			print "Could not connect to database."
-			break
-		c = conn.cursor()
-		c.execute(i)
-		conn.commit()
-		conn.close()
-	print "Database Successfully Created!"
+
+engine = create_engine('sqlite:///ItemCatalog.db')
+Base.metadata.create_all(engine)
--- webserver.py
+++ webserver.py
@@ -1,27 +1,61 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, make_response
+from flask import session as login_session
 import sqlite3
-import time, string, datetime
+import time, string, datetime, random
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+from flask import jsonify
+import requests
+from createDB import Base, Items, Users
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-def connectToDB():
-    '''returns cursur object for ItemCatalog.db 
-    contains the following:
-    TABLE events
-    date text,
-    title text,
-    description text'''
-    conn = sqlite3.connect('ItemCatalog.db')
-    return conn.cursor(), conn
+# Create session and connect to DB
+engine = create_engine('sqlite:///ItemCatalog.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 app = Flask(__name__)
 
-# makes eventsInMonth procedure callable inside templates
+# makes procedure callable inside templates
 # app.jinja_env.globals.update(functionName=functionName)
 
 @app.route('/')
 def LandingPage():
-    return render_template('landingPage.html')
+    saleItems = session.query(Items).filter(Items.onSale == 'True').all()
+    clearanceItems = session.query(Items).filter(Items.onClearance == 'True').all()
+    return render_template('landingPage.html', saleItems=saleItems, clearanceItems=clearanceItems)
+
+@app.route('/newItem', methods=['POST', 'GET'])
+def newItem():
+    return "New Item Page"
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    return render_template('login.html')
+
+@app.route('/<int:userID>/profile')
+def profile(userID):
+    user = session.query(Users).filter(Users.id == userID).one()
+    return render_template('userProfile.html', user=user)
+
+@app.route('/<int:userID>/userSettings')
+def UserSettings(userID):
+    return "user Settings"
+
+@app.route('/<int:userID>/logout')
+def Logout(userID):
+    return "logout"
+
+@app.route('/clearanceItemsAPI')
+def clearanceItemsAPI():
+    clearanceItems = session.query(Items).filter(Items.onClearance == 'True').all()
+    return jsonify(results=[e.serialize() for e in clearanceItems])
 
 
 if __name__ == "__main__":
+    app.secret_key = 'super_secret_key'
     app.run(debug=True, host='127.0.0.1', port=5000)
